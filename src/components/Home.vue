2a4580472d1bcb9e12c55ced7cb6835d307480b3
Unevilicorn <unevilicorn@gmail.com>
2022-01-22 14:42:00 +0000

    centered table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
+    <TextForm />
+    <br />
     <Table />
 </template>
 
 <script>
 import Table from "./Table.vue";
+import TextForm from "./TextForm.vue";
+
 export default {
     components: {
         Table,
+        TextForm,
     },
 };
 </script>
